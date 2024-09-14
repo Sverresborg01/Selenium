5c15ed43ac6af39fc0803b630158b7059a86295d
Sverresborg01 <97440928+Sverresborg01@users.noreply.github.com>
2024-09-15 00:00:45 +0200

    Create main.lua

---
+++ v1/main.lua
@@ -0,0 +1,17 @@
+-- FPS Limiter Settings
+local targetFPS = 0.01 -- Set this to limit FPS (2 FPS in this case)
+local frameDelay = 1 / targetFPS
+
+-- RunService to handle frame updates
+local RunService = game:GetService("RunService")
+local lastUpdate = tick()
+
+RunService.RenderStepped:Connect(function()
+    if (tick() - lastUpdate) >= frameDelay then
+        -- Update frame
+        lastUpdate = tick()
+    else
+        -- Wait to simulate low FPS
+        RunService.RenderStepped:Wait()
+    end
+end)
